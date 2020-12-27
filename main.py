4da0da48258c084bd4ed4635e54d00c8b96e96e8
Nikita “NBANDZEN” <baruzdinn@gmail.com>
2020-12-27 18:28:52 +0000

    запись XLSx

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 import requests
 from bs4 import BeautifulSoup
 result = requests.get('https://pogoda1.ru/katalog/sverdlovsk-oblast/temperatura-vody/2020')
@@ -21,19 +22,28 @@ for i in range(22, 39):
   el2 = boo[boo.find('Р'):boo.find('</'):].replace("()",'').strip()
   name_of_rivers.append(el2)
 
-import csv
-outputfile=open('output.csv','w',newline='')
-outputwriter=csv.writer(outputfile)
+import openpyxl
+wb = openpyxl.Workbook()
+wb.sheetnames
+wb.create_sheet(title='2020', index=0 )
+wb.remove(wb['Sheet'])
+sheet=wb['2020']
+
+
+
+
+#outputfile=open('output.csv','w',newline='')
+#outputwriter=csv.writer(outputfile)
 months.append('Название реки\Месяц')
 river=rivers[0]
 for i in range(12):
   months.append(river[i*2])
-outputwriter.writerow(months)
+sheet.append(months)
 for i in range(len(rivers)):
   river=rivers[i]
   temp_river.append(name_of_rivers[i])
   for j in range(12):
     temp_river.append(river[2*j+1])
-  outputwriter.writerow(temp_river)
+  sheet.append(temp_river)
   temp_river.clear()
-outputfile.close()
+wb.save('Temp.xlsx')
